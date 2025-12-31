36096245bcc98e39a97396689174aad3d6176dfc
R3ndly <akalinart020@gmail.com>
2025-12-31 18:39:17 +0800

    Добавлены новые маршруты

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,7 +2,18 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: () => import('@/views/HomeView.vue')
+    },
+    {
+      path: '/register',
+      name: 'registration',
+      component: () => import('@/views/auth/Register.vue')
+    }
+  ],
 })
 
 export default router
