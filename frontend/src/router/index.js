f2c6f1dc63ccc7ae01b44b92d6e27cc198190e7d
R3ndly <akalinart020@gmail.com>
2026-01-31 16:33:07 +0800

    Изменения роутера фронтенда, добавление защиты маршрутов

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,8 +12,29 @@ const router = createRouter({
       path: '/register',
       name: 'registration',
       component: () => import('@/views/auth/Register.vue')
+    },
+    {
+      path: '/login',
+      name: 'Authentication',
+      component: () => import('@/views/auth/Login.vue')
+    },
+    {
+      path: '/profile',
+      name: 'UserProfile',
+      component: () => import('@/views/Profile.vue'),
+      meta: { requiresAuth: true }
     }
   ],
-})
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+
+  if(requiresAuth && !localStorage.getItem('token')) {
+    next('/login');
+  } else {
+    next();
+  }
+});
+
+export default router;
